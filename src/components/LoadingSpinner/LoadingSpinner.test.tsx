7d520ea5998291154a3c2459cd63299bf774ef07
danbahrami <dan.bahrami@googlemail.com>
2021-07-16 15:48:40 +0100

    Add a comment detailing pros/cons of testing approach

--- src/components/LoadingSpinner/LoadingSpinner.test.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.test.tsx
@@ -1,6 +1,21 @@
 import { render } from '@testing-library/react';
 import { LoadingSpinner } from './LoadingSpinner';
 
+/**
+ * With the <LoadingSpinner /> tests I've taken a much simpler
+ * aproach to the <Button /> tests in that all the tests are
+ * using snapshots.
+ *
+ * The upsides are:
+ *   - It's faster and less verbose to get full test coverage
+ *   - The tests are easier to read
+ *   - Snapshots surface changes to the component really nicely
+ *
+ * The downsides are:
+ *   - The tests don't highlight specific behaviour of the component
+ *   - Snapshots can be accidentally updated without noticing
+ *   - Changes to snapshots might be harder to review
+ */
 describe('Components / LoadingSpinner', () => {
   it('renders a loading spinner', () => {
     const { container } = render(<LoadingSpinner />);
