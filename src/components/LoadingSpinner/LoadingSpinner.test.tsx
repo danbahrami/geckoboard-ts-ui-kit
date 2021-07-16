c8467af06f4032f108d3727979c5bc1ae58fa386
danbahrami <dan.bahrami@googlemail.com>
2021-07-16 15:43:58 +0100

    Simplify Loading Spinner test

--- src/components/LoadingSpinner/LoadingSpinner.test.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.test.tsx
@@ -1,43 +1,21 @@
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
 import { LoadingSpinner } from './LoadingSpinner';
 
 describe('Components / LoadingSpinner', () => {
-  let container: Element;
-
-  beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-  });
-
   it('renders a loading spinner', () => {
-    act(() => {
-      render(<LoadingSpinner />, container);
-    });
+    const { container } = render(<LoadingSpinner />);
 
     expect(container).toMatchSnapshot();
   });
 
   it('lets you pick a size', () => {
-    act(() => {
-      render(<LoadingSpinner size={40} />, container);
-    });
+    const { container } = render(<LoadingSpinner size={40} />);
 
     expect(container).toMatchSnapshot();
   });
 
   it('lets you apply a `dark` theme', () => {
-    act(() => {
-      render(<LoadingSpinner theme="dark" />, container);
-    });
+    const { container } = render(<LoadingSpinner theme="dark" />);
 
     expect(container).toMatchSnapshot();
   });
